f551334e2cde9cfc1a7556524339f4d0f69e7cea
Eli Weinberger <ebweinberger@ElisMBP.fios-router.home>
2020-06-14 13:16:41 -0400

    points plotted on the browser\!

--- sketch.js
+++ sketch.js
@@ -1,5 +1,16 @@
+let socket;
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
+  socket = io.connect('http://localhost:3030')
+
+  socket.on('data',
+  function(coords){
+    let str = coords[0] + ", " + coords[1];
+    text(str, 10, 10);
+    fill(0, 0, 255);
+    circle(coords[0], coords[1], 10);
+  })
 }
 
 function draw(){
