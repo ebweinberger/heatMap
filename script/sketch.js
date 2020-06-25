c0f1e5bef4c50e4cec604cd266cd81a0aaa4b7c3
Eli Weinberger <ebweinberger@ElisMBP.local>
2020-06-25 00:25:44 -0400

    dots now go blue to red instead of shrink

--- script/sketch.js
+++ script/sketch.js
@@ -1,9 +1,19 @@
+//General setup
 let map;
 let tweets = [];
-let FRAMERATE = 10;
-let DOTSIZE = 10;
-let SHRINKRATE = .1;
+let FRAMERATE = 30;
 let MAP_PATH = '../assets/map.png';
+
+//Dot color and size settings
+let TRANSPERENCY = .75;
+let RANDOM_MAX = 255;
+let RANDOM_MIN = 0;
+let DOTSIZE = 10;
+
+//Rates of change
+let COLOR_SHIFT = 1;
+let SHRINKRATE = .05;
+
 
 
 function preload(){
@@ -38,11 +48,13 @@ function draw(){
     var current_tweet = tweets[i]
     var long = (current_tweet.long * windowWidth) / 360;
     var lat = ((current_tweet.lat * -1) * windowHeight) / 180;
-    fill(current_tweet.r, current_tweet.g, current_tweet.b)
+    fill(rgba_string(current_tweet.r, current_tweet.g, current_tweet.b));
     circle(long, lat, current_tweet.size)
-    text(current_tweet.place, long + 10, lat + 5)
-    current_tweet.shrink();
-    current_tweet.isSizeZero();
+    // text(current_tweet.place, long + 10, lat + 5)
+    // current_tweet.shrink();
+    // current_tweet.isSizeZero();
+    current_tweet.shiftColor();
+    current_tweet.isRed();
     if(current_tweet.killFlag){
       tweets.splice(i, 1);
     }
@@ -63,9 +75,12 @@ class tweet{
     this.lat = lat;
     this.place = place;
     this.birthTime = birthTime;
-    this.r = Math.floor(Math.random() * 255);
-    this.g = Math.floor(Math.random() * 255);
-    this.b = Math.floor(Math.random() * 255);
+    // this.r = getRandomInt(RANDOM_MIN, RANDOM_MAX);
+    // this.g = getRandomInt(RANDOM_MIN, RANDOM_MAX);
+    // this.b = getRandomInt(RANDOM_MIN, RANDOM_MAX);
+    this.r = 0;
+    this.g = 0;
+    this.b = 255;
     this.size = size;
     this.killFlag = false;
   }
@@ -74,9 +89,31 @@ class tweet{
     this.size = this.size - SHRINKRATE;
   }
 
+  shiftColor(){
+    this.r = this.r + COLOR_SHIFT;
+    this.b = this.b - COLOR_SHIFT
+  }
+
   isSizeZero(){
     if (this.size < 0.5){
       this.killFlag = true;
     }
   }
+
+  isRed(){
+    if (this.r > 245){
+      this.killFlag = true;
+    }
+  }
+}
+
+function rgba_string(r, g, b){
+  let str = "rgba(" + r + ", " + g + ", " + b + ", " + TRANSPERENCY + ")";
+  return str;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
