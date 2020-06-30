b674e04c0033b3a1eb91a93aa207397b54f30447
Eli Weinberger <ebweinberger@ElisMBP.fios-router.home>
2020-06-30 17:59:44 -0400

    Custom response logs, cant read from aws response

--- api/stream-control.js
+++ api/stream-control.js
@@ -2,51 +2,41 @@ const axios = require('axios');
 const url = require('url');
 
 module.exports = (req, res) => {
+  //Grab the query data from the url
   const query = url.parse(req.url, true).query
 
+  //If the incoming message is "start", post to /start-stream
   if (query.message == "start"){
     axios.post('http://52.14.2.23:3000/start-stream')
     .then((aws_res) => {
-      res.status(200).json(aws_res);
-      console.log(aws_res);
+      res.status(200).send("Stream started");
+      console.log("Stream attempt made!");
     })
     .catch((aws_err) => {
-      res.status(501).send(aws_err);
-      console.log(aws_err);
+      res.status(500).send("Check server logs for error");
+      console.log("Check server logs for error");
     })
-  }else if(query.message == "stop"){
+  }
+  //If the incoming message is "stop", post to /stop-stream
+  else if(query.message == "stop"){
     axios.post('http://52.14.2.23:3000/stop-stream')
     .then((aws_res) => {
       res.status(200).send("Stream stopped");
-      console.log(JSON.stringify(aws_err, censor(aws_err)));
+      console.log("Stream stopped");
     }).catch((aws_err) => {
-      res.status(501).send(aws_err);
-      console.log(JSON.stringify(aws_err, censor(aws_err)));
-    })
-  }else if(query.message == "ping"){
-    axios.post('http://52.14.2.23:3000/ping')
-    .then((aws_res) => {
-      res.status(200).json(aws_res);
-      console.log(aws_res);
-    }).catch((aws_err) => {
-      res.status(501).send(aws_err);
-      console.log(aws_err);
+      res.status(500).send("Check server logs for error");
+      console.log("Check server logs for error");
     })
   }
-}
-
-function censor(censor) {
-  var i = 0;
-
-  return function(key, value) {
-    if(i !== 0 && typeof(censor) === 'object' && typeof(value) == 'object' && censor == value)
-      return '[Circular]';
-
-    if(i >= 29) // seems to be a harded maximum of 30 serialized objects?
-      return '[Unknown]';
-
-    ++i; // so we know we aren't using the original object anymore
-
-    return value;
+  //If the incoming message is "ping", post to /ping
+  else if(query.message == "ping"){
+    axios.post('http://52.14.2.23:3000/ping')
+    .then((aws_res) => {
+      res.status(200).send("Pinged");
+      console.log("Pinged!");
+    }).catch((aws_err) => {
+      res.status(500).send("Check server logs for error");
+      console.log("Check server logs for error");
+    })
   }
 }
