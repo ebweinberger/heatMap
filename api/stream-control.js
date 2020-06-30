59c1197403338d93cb0d20cde51489130f23e41a
Eli Weinberger <ebweinberger@ElisMBP.fios-router.home>
2020-06-30 16:49:39 -0400

    add censor

--- api/stream-control.js
+++ api/stream-control.js
@@ -18,10 +18,10 @@ module.exports = (req, res) => {
     axios.post('http://52.14.2.23:3000/stop-stream')
     .then((aws_res) => {
       res.status(200).send("Stream stopped");
-      console.log(aws_res.toString());
+      console.log(JSON.stringify(aws_err, censor(aws_err)));
     }).catch((aws_err) => {
       res.status(501).send(aws_err);
-      console.log(aws_err);
+      console.log(JSON.stringify(aws_err, censor(aws_err)));
     })
   }else if(query.message == "ping"){
     axios.post('http://52.14.2.23:3000/ping')
@@ -32,5 +32,21 @@ module.exports = (req, res) => {
       res.status(501).send(aws_err);
       console.log(aws_err);
     })
+  }
+}
+
+function censor(censor) {
+  var i = 0;
+
+  return function(key, value) {
+    if(i !== 0 && typeof(censor) === 'object' && typeof(value) == 'object' && censor == value)
+      return '[Circular]';
+
+    if(i >= 29) // seems to be a harded maximum of 30 serialized objects?
+      return '[Unknown]';
+
+    ++i; // so we know we aren't using the original object anymore
+
+    return value;
   }
 }
