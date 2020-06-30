0f6f6fedbaa21a2382b36e422c06d3f558e663af
Eli Weinberger <ebweinberger@ElisMBP.fios-router.home>
2020-06-30 13:05:12 -0400

    logging stream-control

--- api/stream-control.js
+++ api/stream-control.js
@@ -7,23 +7,30 @@ module.exports = (req, res) => {
   if (query.message == "start"){
     axios.post('http://52.14.2.23:3000/start-stream')
     .then((aws_res) => {
-      res.status(200).send(aws_res)
-    }).catch((aws_err) => {
-      res.status(501).send("There was an error: \n" + aws_err)
+      res.status(200).send(aws_res);
+      console.log(aws_res);
+    })
+    .catch((aws_err) => {
+      res.status(501).send(aws_err);
+      console.log(aws_err);
     })
   }else if(query.message == "stop"){
     axios.post('http://52.14.2.23:3000/stop-stream')
     .then((aws_res) => {
-      res.status(200).send(aws_res)
+      res.status(200).send(aws_res);
+      console.log(aws_res);
     }).catch((aws_err) => {
-      res.status(501).send(aws_err)
+      res.status(501).send(aws_err);
+      console.log(aws_err);
     })
   }else if(query.message == "ping"){
     axios.post('http://52.14.2.23:3000/ping')
     .then((aws_res) => {
-      res.status(200).send(aws_res)
+      res.status(200).send(aws_res);
+      console.log(aws_res);
     }).catch((aws_err) => {
-      res.status(501).send(aws_err)
+      res.status(501).send(aws_err);
+      console.log(aws_err);
     })
   }
 }
