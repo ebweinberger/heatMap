342cd1727863c0cf23e82662e2fef4e28dcef8f0
Eli Weinberger <ebweinberger@ElisMBP.fios-router.home>
2020-06-30 12:18:29 -0400

    serverless function post to aws

---
+++ api/stream-control.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+const url = require('url');
+
+module.exports = (req, res) => {
+  const query = url.parse(req.url, true).query
+
+  if (query.message == "start"){
+    axios.post('http://52.14.2.23:3000/start-stream')
+    .then((aws_res) => {
+      res.status(200).send(aws_res))
+    }).catch((aws_err) => {
+      res.status(500).send(aws_err)
+    })
+  }else if(query.message == "stop"){
+    axios.post('http://52.14.2.23:3000/stop-stream')
+    .then((aws_res) => {
+      res.status(200).send(aws_res))
+    }).catch((aws_err) => {
+      res.status(500).send(aws_err)
+    })
+  }else if(query.message == "ping"){
+    axios.post('http://52.14.2.23:3000/ping')
+    .then((aws_res) => {
+      res.status(200).send(aws_res))
+    }).catch((aws_err) => {
+      res.status(500).send(aws_err)
+    })
+  }
+}
